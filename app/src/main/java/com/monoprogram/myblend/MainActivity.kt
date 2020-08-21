08a07b2fc905073a69e5a9348d8c7feed1c9ee13
DESKTOP-U0CV08T\tadashi <common@mono-program.co.jp>
2020-08-21 10:27:33 +0900

    通知バーの色を変更

--- app/src/main/java/com/monoprogram/myblend/MainActivity.kt
+++ app/src/main/java/com/monoprogram/myblend/MainActivity.kt
@@ -2,19 +2,15 @@ package com.monoprogram.myblend
 
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProviders
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.AdView
 import com.google.android.gms.ads.MobileAds
-import com.monoprogram.myblend.entity.Herb
 import com.monoprogram.myblend.presentation.top.blend.MyRecipeViewModel
 import com.monoprogram.myblend.presentation.top.blend.amount.BlendAmountFragment
 import com.monoprogram.myblend.presentation.top.blend.top.TopFragment
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 
@@ -30,7 +26,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         viewModel = ViewModelProviders.of(this).get(MyRecipeViewModel::class.java)
-
+        findViewById<View>(android.R.id.content).systemUiVisibility =
+            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
         // 広告の初期化
         Application.component.inject(this)
         MobileAds.initialize(this) {}
