0214eb46452347243ec4f4a534275e639111d455
DESKTOP-U0CV08T\tadashi <common@mono-program.co.jp>
2020-08-29 18:45:47 +0900

    グラム確認機能を追加

---
+++ app/src/main/java/com/monoprogram/myblend/presentation/top/blend/amount/AmountConfirmAdapter.kt
@@ -0,0 +1,52 @@
+package com.monoprogram.myblend.presentation.top.blend.amount
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.monoprogram.myblend.R
+import com.monoprogram.myblend.entity.Herb
+
+
+class AmountConfirmAdapter internal constructor(
+    private var herbInfo: ArrayList<Herb>,
+    private var itemValues: ArrayList<Float>
+) :
+    RecyclerView.Adapter<AmountConfirmAdapter.ViewHolder>() {
+
+    class ViewHolder(v: View) : RecyclerView.ViewHolder(v) {
+        var imageView: ImageView = v.findViewById(R.id.image_herb)
+        var textView: TextView = v.findViewById(R.id.text_amount)
+    }
+
+    // Create new views (invoked by the layout manager)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        // create a new view
+        val view: View = LayoutInflater.from(parent.context)
+            .inflate(R.layout.amount_blend_item, parent, false)
+
+        // set the view's size, margins, paddings and layout parameters
+        return ViewHolder(
+            view
+        )
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.imageView.setImageResource(herbInfo[position].imageId)
+        holder.textView.text = itemValues[position].toString() + "g"
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    override fun getItemCount(): Int {
+        return herbInfo.size
+    }
+
+    fun updateValue(update: ArrayList<Float>) {
+        itemValues = update
+        notifyDataSetChanged()
+    }
+
+}
--- app/src/main/java/com/monoprogram/myblend/presentation/top/blend/amount/AmountConfirmFragment.kt
+++ app/src/main/java/com/monoprogram/myblend/presentation/top/blend/amount/AmountConfirmFragment.kt
@@ -1,21 +1,28 @@
 package com.monoprogram.myblend.presentation.top.blend.amount
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.monoprogram.myblend.R
 import com.monoprogram.myblend.databinding.FragmentAmountConfirmBinding
+import com.monoprogram.myblend.entity.Herb
 import com.monoprogram.myblend.presentation.top.blend.MyRecipeViewModel
 
 class AmountConfirmFragment : Fragment() {
 
     private lateinit var viewModel: MyRecipeViewModel
+    private lateinit var binding: FragmentAmountConfirmBinding
+    private lateinit var adapter: AmountConfirmAdapter
     private var herbList: ArrayList<String> = arrayListOf()
     private var valueList: ArrayList<Int> = arrayListOf()
-    private lateinit var binding: FragmentAmountConfirmBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,9 +47,59 @@ class AmountConfirmFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(MyRecipeViewModel::class.java)
 
+        viewModel.onSelectedHerbList(herbList)
+
         binding.btnBack.setOnClickListener {
             activity?.onBackPressed()
         }
+
+        viewModel.herbInfo.observe(viewLifecycleOwner, Observer {
+            createAdapter(it as ArrayList<Herb>)
+        })
+
+        binding.editCups.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {
+                var input: Int
+                p0.toString().let {
+                    input = if (it.isEmpty())
+                        0
+                    else
+                        it.toInt()
+                }
+                adapter.updateValue(calcCups(valueList, input))
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                // nop
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                // nop
+            }
+
+        })
+    }
+
+    private fun createAdapter(herbList: ArrayList<Herb>) {
+        val recyclerView =
+            view?.findViewById<RecyclerView>(R.id.amount_recycler_view) ?: return
+        adapter =
+            AmountConfirmAdapter(
+                herbList,
+                calcCups(valueList, 10)
+            )
+        recyclerView.setHasFixedSize(true)
+        recyclerView.layoutManager = GridLayoutManager(activity, 2)
+        recyclerView.adapter = adapter
+        adapter.notifyDataSetChanged()
+    }
+
+    private fun calcCups(valList: ArrayList<Int>, calc: Int): ArrayList<Float> {
+        val retList: ArrayList<Float> = arrayListOf()
+        valList.forEach {
+            retList.add((it / 10F) * 3F * calc)
+        }
+        return retList
     }
 
     companion object {
